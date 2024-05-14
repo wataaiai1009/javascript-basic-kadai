a91be14fd86545a67699a6b5d74e7c3980f8ca9c
wataaiai1009 <169005311+wataaiai1009@users.noreply.github.com>
2024-05-14 13:57:13 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,14 @@
+// btnというidをもつHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// textというidをもつHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+  text.textContent = 'ボタンをクリックしました';
+});
+
+
+
+
